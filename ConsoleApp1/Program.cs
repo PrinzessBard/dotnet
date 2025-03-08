7a3d76b35b7ad00529e568fc47c6674aa4aa4c7a
PrinzessBard <egorholikov234@gmail.com>
2025-03-08 12:02:35 +0300

    Commit array lesson

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,7 @@
-﻿int i = 0;
-++i;
-Console.WriteLine(i); 
+﻿int[] cucumbers = new int[3];
+cucumbers[2] = 13;
+
+for (int i = 0; i < cucumbers.Length; i++)
+{
+    Console.WriteLine(cucumbers[i]);
+}
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95fbe39273254c667b54b9ae78ce990af0d0242")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
